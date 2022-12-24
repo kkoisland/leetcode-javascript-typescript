bedca42b067ce0a126ca3790d5fd57e9ca1a9017
Keiko Higuchi <keiko_higuchi@claris.com>
2022-12-23 22:20:15 -0800

    add 0001 Two Sum

--- 0001 Two Sum.js
+++ 0001 Two Sum.js
@@ -6,7 +6,20 @@ You can return the answer in any order.
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- 
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
 Start from: 
 var twoSum = function(nums, target) {
  */
@@ -25,15 +38,15 @@ var twoSum = function(nums, target) {
   };
   console.log(twoSum([3,2,4], 7)); //[0,2]
   
-  // Solution #2 better done by Akih
-  var twoSum = function(nums, target) { 
-    return nums.reduce((acc, cur, i) => {
-      for (let j = i + 1; j < nums.length; j++ ) {
-        if (cur + nums[j] === target) {
-           nums.length = 0;
-          return [i, j];
-        }
+// Solution #2 better done by Akih
+var twoSum = function(nums, target) { 
+  return nums.reduce((acc, cur, i) => {
+    for (let j = i + 1; j < nums.length; j++ ) {
+      if (cur + nums[j] === target) {
+          nums.length = 0;
+        return [i, j];
       }
-  },[]);
-  };
-  console.log(twoSum([3,2,4], 7)); //[0,2]
+    }
+},[]);
+};
+console.log(twoSum([3,2,4], 7)); //[0,2]
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # leetcode-javascript-typescript
 
+Use [codepen](https://codepen.io/) to see console.log() result
+
 [60 questions to solve](https://leetcode.com/list/xo2bgr0r/)
 
 [0001 Two Sum](https://leetcode.com/problems/two-sum/) (easy)
