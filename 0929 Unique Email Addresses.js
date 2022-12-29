136b2ba1a4fbbff4084729a56ed6fafcb4b5b94f
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2022-12-29 11:22:07 -0800

    modify validate

--- 0929 Unique Email Addresses.js
+++ 0929 Unique Email Addresses.js
@@ -49,5 +49,5 @@ console.log(numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cat
 console.log(numUniqueEmails(["a@leetcode.com","b@leetcode.com","c@leetcode.com"])); // 3
 console.log(numUniqueEmails(["test.email+alex@lee.tc.o+d.e.com"])); // 1
 console.log(numUniqueEmails(["fg.r.u.uzj+o.pw@kziczvh.com","r.cyo.g+d.h+b.ja@tgsg.z.com","fg.r.u.uzj+o.f.d@kziczvh.com","r.cyo.g+ng.r.iq@tgsg.z.com","fg.r.u.uzj+lp.k@kziczvh.com","r.cyo.g+n.h.e+n.g@tgsg.z.com","fg.r.u.uzj+k+p.j@kziczvh.com","fg.r.u.uzj+w.y+b@kziczvh.com","r.cyo.g+x+d.c+f.t@tgsg.z.com","r.cyo.g+x+t.y.l.i@tgsg.z.com","r.cyo.g+brxxi@tgsg.z.com","r.cyo.g+z+dr.k.u@tgsg.z.com","r.cyo.g+d+l.c.n+g@tgsg.z.com","fg.r.u.uzj+vq.o@kziczvh.com","fg.r.u.uzj+uzq@kziczvh.com","fg.r.u.uzj+mvz@kziczvh.com","fg.r.u.uzj+taj@kziczvh.com","fg.r.u.uzj+fek@kziczvh.com"])); //2
-console.log(numUniqueEmails(["fg.r.u.uzj+o.pw@kziczvh.com","r.cyo.g+d.h+b.ja@tgsg.z.com","fg.r.u.uzj+o.f.d@kziczvh.com","r.cyo.g+ng.r.iq@tgsg.z.com", "keiko@kkoisland.com"])); //3
+console.log(numUniqueEmails(["fg.r.u.uzj+o.pw@kziczvh.com","r.cyo.g+d.h+b.ja@tgsg.z.com","fg.r.u.uzj+o.f.d@kziczvh.com","r.cyo.g+ng.r.iq@tgsg.z.com", "keiko@leetcode.com"])); //3
 console.log(numUniqueEmails(["test.emailalex@leetcode.com","test.e.mailbob.cathy@leetcode.com","testemail+david@lee.tcode.com"])); // 3
