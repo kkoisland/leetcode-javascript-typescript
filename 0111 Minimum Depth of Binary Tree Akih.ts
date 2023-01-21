0edf70bf99488602a5f4ef610b8148a9d2bececb
Akihiro Nishiyama <akih@keroling.net>
2023-01-21 10:06:41 -0800

    my solution

---
+++ 0111 Minimum Depth of Binary Tree Akih.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function minDepth(root: TreeNode | null): number {
+    let l: number = 0; 
+    let r: number = 0;
+    if (!root) return 0;
+    if (!root.left && !root.right) return 1
+    if (root.left && !root.left.left && !root.left.right ||
+        root.right && !root.right.left && !root.right.right) {
+        return 2;
+    }
+    if (root.left) l = minDepth(root.left); 
+    if (root.right) r = minDepth(root.right);
+    if (!l) l = r;
+    if (!r) r = l;
+    return Math.min(l, r) + 1;
+};
+
