55514672fdfb0b84912090a1161960c0540f3430
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2023-01-28 13:57:16 -0800

    add leetcode on 2023/1/28

--- 0002 Add Two Numbers.js
+++ 0002 Add Two Numbers.js
@@ -28,6 +28,83 @@ function ListNode(val, next) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+// Final - Thanks to Akih! Runtime  102 ms  Beats  86.26%  Memory  47.3 MB  Beats  44.61%
+var addTwoNumbers = function (l1, l2) {
+  let node1 = l1;
+  let node2 = l2;
+  let startNode = new ListNode(0);
+
+  let kuriagari = 0;
+  resNode = startNode;
+  while (node1 !== null || node2 !== null) {
+    const calc = (node1 ? node1.val : 0) + (node2 ? node2.val : 0);
+
+    resNode.val = (calc + kuriagari) % 10;
+    if (calc + kuriagari > 9) {
+      resNode.next = new ListNode(1);
+      kuriagari = 1;
+    } else if ((!node1?.next || !node1) && (!node2?.next || !node2)) {
+      resNode.next = null;
+    } else {
+      resNode.next = new ListNode(0);
+      kuriagari = 0;
+    }
+    resNode = resNode.next;
+
+    node1 = node1 ? node1.next : null;
+    node2 = node2 ? node2.next : null;
+  }
+  return startNode;
+};
+
+// 3rd try: almost success, but cannot add null at the end properly
+var addTwoNumbers = function (l1, l2) {
+  let node1 = l1;
+  let node2 = l2;
+  let startNode = new ListNode(0);
+
+  let kuriagari = 0;
+  resNode = startNode;
+
+  while (node1 !== null && node2 !== null) {
+    if (node2 !== null) {
+      calc = node1.val + node2.val;
+      node2 = node2.next;
+    } else if (node1 === null && node2 !== null) {
+      calc = node2.val;
+      node2 = node2.next;
+    }
+
+    let result = calc;
+
+    if (kuriagari === 1) {
+      result = result + kuriagari;
+      kuriagari--;
+    }
+    if (result > 9) {
+      result = result - 10;
+      kuriagari++;
+    }
+
+    resNode.val = result;
+
+    if (node1?.next === null && node2?.next === null) resNode.next = null;
+    else {
+      resNode.next = new ListNode(0);
+      resNode = resNode.next;
+    }
+
+    if (kuriagari === 1) {
+      resNode.next = new ListNode(1);
+    }
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+  return startNode;
+};
+
+// 2nd try
 var addTwoNumbers = function (l1, l2) {
   let node2 = l2;
   let startNode = new ListNode(0);
@@ -65,76 +142,30 @@ var addTwoNumbers = function (l1, l2) {
   return startNode;
 };
 
-// var addTwoNumbers = function (l1, l2) {
-//   let node2 = l2;
-//   let startNode = new ListNode(0);
-
-//   let kuriagari = 0;
-//   resNode = startNode;
-//   for (let node1 = l1; node1 !== null || node2 !== null; node1 = node1.next) {
-//     let calc = node1.val;
-//     if (node2 !== null) {
-//       calc = node1.val + node2.val;
-//       node2 = node2.next;
-//     }
-//     let result = calc;
-
-//     console.log(`kuriagari: ${kuriagari}`);
-//     console.log(`result: ${result}`);
-//     console.log("-----------------------------");
-
-//     if (kuriagari === 1) {
-//       result = result + kuriagari;
-//       kuriagari--;
-//     }
-//     if (calc > 9) {
-//       result = calc - 10;
-//       kuriagari++;
-//     }
-//     console.log(`kuriagari: ${kuriagari}`);
-//     console.log(`result: ${result}`);
-//     console.log("----------------------------------------------------------");
-//     resNode.val = result;
-//     if (node1.next === null) resNode.next = null;
-//     else {
-//       resNode.next = new ListNode(0);
-//       resNode = resNode.next;
-//     }
-//   }
-//   return startNode;
-// };
-
 // 1st try
-// var addTwoNumbers = function(l1, l2) {
-//     let num1 = [];
-//     for (let node=l1; node !== null; node = node.next){
-//         num1.push(node.val);
-//     }
-//     const revnum1 = Number(num1.reverse().join('')); // 342
-//     let num2 = [];
-//     for (let node=l2; node !== null; node = node.next){
-//         num2.push(node.val);
-//     }
-//     const revnum2 = Number(num2.reverse().join('')); // 465
-//     const resnum = (revnum1 + revnum2).toString().split('').reverse(); // ['7', '0', '8']
+var addTwoNumbers = function(l1, l2) {
+    let num1 = [];
+    for (let node=l1; node !== null; node = node.next){
+        num1.push(node.val);
+    }
+    const revnum1 = Number(num1.reverse().join('')); // 342
+    let num2 = [];
+    for (let node=l2; node !== null; node = node.next){
+        num2.push(node.val);
+    }
+    const revnum2 = Number(num2.reverse().join('')); // 465
+    const resnum = (revnum1 + revnum2).toString().split('').reverse(); // ['7', '0', '8']
 
-//     // const resNode = new ListNode(resnum[0]);
-//     // const resNode2 = new ListNode(resnum[1]);
-//     // resNode.next = resNode2;
-//     // const resNode3 = new ListNode(resnum[2]);
-//     // resNode2.next = resNode3;
-//     // resNode3.next = null;
-
-//     const resNode = new ListNode(resnum[0]);
-//     let prevNode = resNode;
-//     for (let i = 1; i<resnum.length; i++){
-//         const resNode = new ListNode(resnum[i]);
-//         prevNode.next = resNode ? resNode : null;
-//         prevNode = resNode;
-//     }
+    const resNode = new ListNode(resnum[0]);
+    let prevNode = resNode;
+    for (let i = 1; i<resnum.length; i++){
+        const resNode = new ListNode(resnum[i]);
+        prevNode.next = resNode ? resNode : null;
+        prevNode = resNode;
+    }
     
-//     return resNode;  
-// };
+    return resNode;  
+};
 
 //Validate
 const l1Node1 = new ListNode(2);
