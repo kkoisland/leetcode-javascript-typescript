05c2c1563f943e6890d4f699683f22d1e64cd33b
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2023-02-18 20:07:06 -0800

    add leetcode on 2023/2/18

--- 0003 Longest Substring Without Repeating Characters.js
+++ 0003 Longest Substring Without Repeating Characters.js
@@ -25,28 +25,34 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const arr = [];
+  let arr = [];
   let len = 0;
   for (i = 0; i < s.length; i++) {
-    console.log(s[i]);
-    for (j = 0; j < arr.length; j++) {
-      console.log(`arr[j]: ${arr[j]}`);
-      console.log(`s[i]: ${s[i]}`);
-      console.log("================");
-      // if (arr.length > 1) {
-      //   if (arr.pop() === s[i]) break;
-      // }
-      if (arr[j] === s[i] && len < j + 1) {
-        len = j + 1;
-        console.log(`len: ${len}`)
-        break;
+    arr = [];
+    // console.log(`i: ${i}`)
+    for (j = i; j < s.length; j++) {
+      let found = arr.some((ar) => ar === s[j]);
+      if (!found) {
+        arr.push(s[j]);
+        // console.log(`push s[j]: ${s[j]}`)
+        // console.log(`arr: ${arr}`)
+      }
+      if (arr.length > len) {
+        len = arr.length;
+        // console.log(len)
+      }
+      if (found){
+        break
       }
     }
-    arr.push(s[i]);
   }
   return len;
 };
 
-// console.log(lengthOfLongestSubstring("pww")); // 3 (wke)
+console.log(lengthOfLongestSubstring("aab")); // 2 (ab)
 console.log(lengthOfLongestSubstring("pwwkew")); // 3 (wke)
-// console.log(lengthOfLongestSubstring("bbbb")); // 1 (b)
+console.log(lengthOfLongestSubstring("abacdea")); // 5
+console.log(lengthOfLongestSubstring("pww")); // 2 (pw)
+console.log(lengthOfLongestSubstring("bbbb")); // 1 (b)
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3 (abc)
+console.log(lengthOfLongestSubstring("ab")); // 2 (ab)
